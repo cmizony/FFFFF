cd75af3ab53231ff4c1ee9a7a50fd0011acfacdc
cmizony <cmizony@gmail.com>
2021-03-03 17:24:58 -0800

    Import betty

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 
 const yargs = require('yargs/yargs');
 const fffffmpeg = require('./lib/fffffmpeg');
+const data = require('./data/default.json');
 
-console.log(fffffmpeg.toBe("test"));
+console.log(fffffmpeg.generateFFMPEG(data));
