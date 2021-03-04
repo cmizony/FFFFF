89ec2fcb99c200326249c1e4815fe1b04aa39a38
cmizony <cmizony@gmail.com>
2021-03-03 20:19:51 -0800

    add yargs options

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 #!/usr/bin/env node
 
-const yargs = require('yargs/yargs');
+const yargs = require('yargs');
 const fffffmpeg = require('./lib/fffffmpeg');
-const data = require('./data/default.json');
 
-console.log(fffffmpeg.generateFFMPEG(data));
+yargs
+.command({
+  command: 'render',
+  describe: 'Renders a video',
+  builder: {
+    input: {
+      describe: 'Movie input (*.mp4) 1920x1080',
+      // demandOption: true,
+      type: 'string'
+    },
+    logo: {
+      describe: 'Logo overlay (*.png)',
+      type: 'string'
+    },
+    config: {
+      describe: 'JSON Config file (see data/default.json)',
+      default: './data/default.json',
+      type: 'string'
+    },
+  },
+
+  handler(argv) {
+    const config = require(argv.config);
+    config.inputFile = argv.input || config.inputFile;
+    config.inputLogo = argv.logo || config.inputLogo;
+
+    const ffmpegOptions = fffffmpeg.generateFFMPEG(config);
+    const command = `yes | ./bin/ffmpeg/ffmpeg ${ffmpegOptions}`;
+
+    console.log(command);
+  }
+})
+.alias('i', 'input')
+.alias('l', 'logo')
+.alias('f', 'config')
+.nargs('i', 1)
+
+yargs.parse()
